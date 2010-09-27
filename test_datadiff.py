177acee94c90493971f596bd4a211ac2373562d1
Dave Brondsema <dave@brondsema.net>
2010-09-27 00:57:27 -0400

    dicts, better list test

--- test_datadiff.py
+++ test_datadiff.py
@@ -5,14 +5,18 @@ from nose.tools import assert_equal
 from datadiff import diff
 
 def test_diff_list():
-    a = [1,2,3]
-    b = [1,2]
+    a = [1,'x', 2, 3, 4]
+    b = [1,'y', 2, 4, 5]
     d = diff(a, b)
     expected = dedent('''\
         [
          1,
+        -'x',
+        +'y',
          2,
         -3,
+         4,
+        +5,
         ]''')
     print d
     print expected
@@ -32,8 +36,18 @@ def test_diff_list():
 """
  
 def test_diff_dict():
-    a = dict(one=1, two=2)
-    b = dict(one=1, three=3)
+    a = dict(zero=0, one=1, two=2, three=3)
+    b = dict(zero='@', one=1, three=3, four=4)
     d = diff(a, b)
     print d
-    assert_equal(str(d), None)
+    expected = dedent('''\
+        {
+        +'four': 4,
+        -'zero': 0,
+        +'zero': '@',
+         'three': 3,
+         'one': 1,
+        -'two': 2,
+        }''')
+    print expected
+    assert_equal(str(d), expected)
