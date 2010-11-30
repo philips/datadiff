a3e4aa8d8ea6ff27191624e1109447266ccaeb81
Dave Brondsema <dave@brondsema.net>
2010-11-30 16:50:40 -0500

    start datadiff.tools

---
+++ datadiff/tools.py
@@ -0,0 +1,31 @@
+"""
+Copyright 2010 Dave Brondsema
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import logging
+log = logging.getLogger('datadiff.tools')
+
+from datadiff import diff
+
+# drop-in replacements for http://somethingaboutorange.com/mrl/projects/nose/doc/module_nose.tools.html
+
+def assert_equal(first, second, msg=None):
+    if first == second:
+        return
+    if msg is None:
+        msg = diff(first, second)    
+    raise AssertionError(msg)
+
+assert_equals = assert_equal
---
+++ test_datadiff_tools.py
@@ -0,0 +1,59 @@
+import sys
+from textwrap import dedent
+
+from datadiff import tools
+
+from test_datadiff import assert_equal
+
+
+def test_assert_equal_true():
+    # nothing raised
+    assert_equal(None, tools.assert_equals(7, 7))
+    
+def test_assert_equal_false():
+    try:
+        tools.assert_equals([3,4], [5,6])
+    except:
+        e = sys.exc_info()[1]
+        assert_equal(type(e), AssertionError)
+        assert_equal(str(e), dedent('''\
+            --- a
+            +++ b
+            [
+            @@ -0,1 +0,1 @@
+            -3,
+            -4,
+            +5,
+            +6,
+            ]'''))
+    else:
+        raise AssertionError("Should've raised an AssertionError")
+    
+def test_assert_equal_msg():
+    try:
+        tools.assert_equals(3, 4, "whoops")
+    except:
+        e = sys.exc_info()[1]
+        assert_equal(type(e), AssertionError,
+                     "Raised exception should be AssertionError")
+        assert_equal(str(e), "whoops")
+    else:
+        raise AssertionError("Should've raised an AssertionError")
+    
+def test_assert_equals():
+    assert_equal(tools.assert_equal, tools.assert_equals)
+
+if __name__ == '__main__':
+    try:
+        import nose
+    except (ImportError, SyntaxError, NameError):
+        pass
+    else:
+        nose.main()
+        sys.exit(0)
+    
+    import types
+    for fn_name, fn in sorted(locals().items()):
+        if fn_name.startswith('test_') and type(fn) == types.FunctionType:
+            sys.stderr.write(fn_name + '...\n')
+            fn()
